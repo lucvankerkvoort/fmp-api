0be69e627d1918a4560545405ef94e265b802988
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-10-07 12:22:51 -0700

    changed the database variables

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "mongoose",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "mongo",
+        uri: `${process.env.DATABASE_URI}`,
       },
       options: {
-        useNullAsDefault: true,
+        ssl: true,
       },
     },
   },
