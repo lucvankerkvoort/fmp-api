ff83f074bb85648c81fa828b5e94787c10e052b3
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-10-14 03:59:15 -0700

    added all the changes for an import

--- plugins/import-content/admin/src/containers/HomePage/index.js
+++ plugins/import-content/admin/src/containers/HomePage/index.js
@@ -22,10 +22,71 @@ const getUrl = to =>
   to ? `/plugins/${pluginId}/${to}` : `/plugins/${pluginId}`;
 
 class HomePage extends Component {
+
+
+  importSources = [
+    { label: "External URL ", value: "url" },
+    { label: "Upload file", value: "upload" },
+    { label: "Raw text", value: "raw" }
+  ];
+
   state = {
+    loading: true,
+    modelOptions: [],
+    models: [],
+    importSource: "upload",
     analyzing: false,
-    analysis: null
+    analysis: null,
+    selectedContentType: "" // <---
   };
+
+
+  componentDidMount() {
+    this.getModels().then(res => {
+    const { models, modelOptions } = res;
+    this.setState({
+      models,
+      modelOptions,
+      selectedContentType: modelOptions ? modelOptions[0].value : ""
+    });
+  });
+}
+
+selectImportDest = selectedContentType => {
+  this.setState({ selectedContentType });
+};
+  getModels = async () => {
+    this.setState({ loading: true });
+    try {
+	  const response = await request("/content-type-builder/content-types", {
+        method: "GET"
+      });
+
+      // Remove non-user content types from models
+      const models = get(response, ["data"], []).filter(
+        obj => !has(obj, "plugin")
+      );
+      const modelOptions = models.map(model => {
+        return {
+          label: get(model, ["schema", "name"], ""), // (name is used for display_name)
+          value: model.uid // (uid is used for table creations)
+        };
+      });
+
+      this.setState({ loading: false });
+
+      return { models, modelOptions };
+    } catch (e) {
+      this.setState({ loading: false }, () => {
+        strapi.notification.error(`${e}`);
+      });
+    }
+    return [];
+  };
+
+  selectImportSource = importSource => {
+    this.setState({ importSource });
+ };
 
   onRequestAnalysis = async analysisConfig => {
     this.analysisConfig = analysisConfig;
@@ -49,39 +110,63 @@ class HomePage extends Component {
   };
 
 render() {
-   return (
+  return (
     <div className={"container-fluid"} style={{ padding: "18px 30px" }}>
-    <PluginHeader
-      title={"Import Content"}
-      description={"Import CSV and RSS-Feed into your Content Types"}
-    />
-    <HeaderNav
-      links={[
-        {
-          name: "Import Data",
-          to: getUrl("")
-        },
-        {
-          name: "Import History",
-          to: getUrl("history")
-        }
-      ]}
-      style={{ marginTop: "4.4rem" }}
-    />
-    <div className="row">
-      <Block
-        title="General"
-        description="Configure the Import Source & Destination"
-        style={{ marginBottom: 12 }}
-      >
-        <UploadFileForm
-          onRequestAnalysis={this.onRequestAnalysis}
-          loadingAnalysis={this.state.analyzing}
-        />
-      </Block>
+      <PluginHeader
+        title={"Import Content"}
+        description={"Import CSV and RSS-Feed into your Content Types"}
+      />
+      <HeaderNav
+        links={[
+          {
+            name: "Import Data",
+            to: getUrl("")
+          },
+          {
+            name: "Import History",
+            to: getUrl("history")
+          }
+        ]}
+        style={{ marginTop: "4.4rem" }}
+      />
+      <div className="row">
+        <Block
+          title="General"
+          description="Configure the Import Source & Destination"
+          style={{ marginBottom: 12 }}
+        >
+          <Row className={"row"}>
+            <div className={"col-4"}>
+              <Label htmlFor="importSource">Import Source</Label>
+              <Select
+  name="importSource"
+  options={this.importSources}
+  value={this.state.importSource}
+  onChange={({ target: { value } }) =>
+    this.selectImportSource(value)
+  }
+/>
+            </div>
+            <div className={"col-4"}>
+    <Label htmlFor="importDest">Import Destination</Label>
+    <Select
+  value={this.state.selectedContentType}
+  name="importDest"
+  options={this.state.modelOptions}
+  onChange={({ target: { value } }) =>
+    this.selectImportDest(value)
+  }
+/>
+</div>
+          </Row>
+          <UploadFileForm
+            onRequestAnalysis={this.onRequestAnalysis}
+            loadingAnalysis={this.state.analyzing}
+          />
+        </Block>
+      </div>
     </div>
-  </div>
-    );
+  );
 }
 }
 export default memo(HomePage);
