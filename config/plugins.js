4070b75fd3e55d170b30fcf30412b5171de595cf
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-11-04 17:14:49 -0800

    added the correct data to the plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     providerOptions: {
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
       secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: env("AWS_REGION"),
+      region: env("AWS_BUCKET"),
       params: {
         Bucket: env("AWS_BUCKET"),
       },
