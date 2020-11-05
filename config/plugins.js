566f68e1017e2636a604a6df3f37ef956d18e6c2
Luc van Kerkvoort <luc.van.kerkvoort@gmail.com>
2020-11-04 17:32:26 -0800

    added the correct region to .env and changed the params for the bucket

--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
     providerOptions: {
       accessKeyId: env("AWS_ACCESS_KEY_ID"),
       secretAccessKey: env("AWS_ACCESS_SECRET"),
-      region: env("AWS_BUCKET"),
+      region: env("AWS_REGION"),
       params: {
         Bucket: env("AWS_BUCKET"),
       },
